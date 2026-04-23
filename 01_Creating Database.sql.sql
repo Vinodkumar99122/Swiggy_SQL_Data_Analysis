3656266e4bf208c7a312d1bfb879968299abfa45
M.Vinod Kumar <VinodkumarMandla0@gmail.com>
2026-04-23 11:36:05 +0530

    Update 01_Creating Database.sql

--- 01_Creating Database.sql.sql
+++ 01_Creating Database.sql.sql
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------
-	Script Name : 01_Create_Database.sql
+	Script Name : 01_Create_Database
 	Purpose     : Creates the Swiggy_Project database
 	Description : This script creates the database that will be used
 				  to store Swiggy restaurant data for analysis
